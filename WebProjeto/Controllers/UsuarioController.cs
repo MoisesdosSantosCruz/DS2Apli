efc4eabc8d9c95c3445a947d2e0e681aaeea9404
2DS <2ds@bglab.local>
2023-09-01 12:21:09 -0300

    Primeira alteração

---
+++ WebProjeto/Controllers/UsuarioController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProjeto.Models;
+
+namespace WebProjeto.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            
+            var cliente = new Cliente();
+            return View(cliente);
+
+        }
+        [HttpPost]
+        public ActionResult Index(Cliente cliente)
+        {
+            if(ModelState.IsValid)
+            {
+
+                return View("Resultado", cliente);
+
+            }
+            return View(cliente);
+
+        }
+
+        public ActionResult Resultado(Cliente cliente)
+        {
+
+            return View(cliente);
+
+        }
+    }   
+}
---
+++ WebProjeto/Models/Cliente.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+
+namespace WebProjeto.Models
+{
+    public class Cliente
+    {
+        [Display(Name ="Código")]
+        
+        public int Codcli { get; set; }
+
+
+        [Display(Name = "Nome de Usuário")]
+        [Required(ErrorMessage = "Por favor, preencha o campo Nome")]
+        [StringLength(50, MinimumLength = 3)]
+
+        public string Nome { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage ="Por favor, preencha o campo CPF")]
+        [StringLength(50, MinimumLength = 3)]
+        [DisplayFormat(DataFormatString = "(xxx-xxx-xxx-yy)")]
+
+        public int CPF { get; set; }
+
+        [Display(Name = "Data de Nacimento")]
+        [Required(ErrorMessage = "Por favor, preencha o campo DataNasc")]
+        [DisplayFormat(DataFormatString = "dd/mm/yyyy")]
+
+        public DateTime DataNasc { get; set; }
+
+        [Display(Name = "Endereço")]
+        [Required(ErrorMessage = "Por favor, preencha o campo Endereço")]
+        [StringLength(50, MinimumLength = 3)]
+        
+        public string Endereco { get; set; }
+        
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "Por favor, preencha o campo Telefone")]
+        [StringLength(11)]
+        [DisplayFormat(DataFormatString = "(xx)xxxxx-xxxx")]
+        
+        public string Telefone { get; set; }
+
+        [Display(Name = "Email")]
+        [Required(ErrorMessage ="Por favor, preencha o campo Email")]
+        [RegularExpression("@\"\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*\" ")]
+        
+        public string Email {get; set; }
+
+    }
+}
